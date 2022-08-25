5891e9bb7891f21a82e3b7e417d4399ad48d5d5a
Maurizio <mauriziodm91@gmail.com>
2022-08-25 13:16:35 -0300

    added links to footer icons

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -21,8 +21,14 @@ const Footer = () => {
 
       <footer className='p-4  md:px-6 md:py-8'>
         <span className='flex justify-center text-xl text-gray-500 sm:text-center dark:text-gray-400'>
-          Made With <FaReact className='text-blue-600 mx-2 mt-1' /> &{' '}
-          <SiTailwindcss className='text-sky-400 mx-2 mt-1' />
+          Made With{' '}
+          <a href='https://es.reactjs.org/'>
+            <FaReact className='text-blue-600 mx-2 mt-1' />
+          </a>{' '}
+          &{' '}
+          <a href='https://tailwindcss.com/'>
+            <SiTailwindcss className='text-sky-400 mx-2 mt-1' />
+          </a>
         </span>
         <span className='flex justify-center'>
           <p className='ml-4'>&#169; {getCurrentYear()}</p>
